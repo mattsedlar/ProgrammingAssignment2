d0a01363527e04dd43d002420e997cf1116d97ff
mattsedlar <sedlarm@gmail.com>
2015-06-19 12:11:45 -0700

    Tested functions with matrix(rnorm(100),nrow=2), works

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Here we are caching a matrix and binding the values of x (the matrix) and s (solve) in functions stored in a list
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+## This function gets the stored value of s and determines whether it is cached or not
+## If it is (not null), it returns s. If not, is calls solve and stores the value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data,...)
+  x$setsolve(s)
+  s
+  
 }
